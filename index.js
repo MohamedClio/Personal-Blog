320c166529f6984f81803071ef186cbc6bb1ffa1
@herrmclio <herrmclio@gmail.com>
2024-03-30 02:19:03 +0100

    adding header/footer design and logic

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs");
+});
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
